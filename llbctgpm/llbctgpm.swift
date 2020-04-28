e808cb04faed6114cde7e06f5edbbe07d194444e
Mihaly Bezzeg <mihaly@brainsandbeards.com>
2020-04-28 21:46:36 +0200

    Added test cases

---
+++ llbctgpm/llbctgpm.swift
@@ -0,0 +1,14 @@
+//
+//  llbctgpm.swift
+//  llbctgpm
+//
+//  Created by Bezzeg Mihály on 2020. 04. 28..
+//  Copyright © 2020. Bezzeg Mihály. All rights reserved.
+//
+
+import Foundation
+public final class llbctgpm {
+    public func generateRandomInt() -> Int {
+        Int.random(in: 0 ... 100)
+    }
+}
