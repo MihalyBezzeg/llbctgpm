ee67c4d201d91e34f5f93596051859ffad2c3426
Mihaly Bezzeg <mihaly@brainsandbeards.com>
2020-04-28 21:34:49 +0200

    first commit

---
+++ llbctgpmExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  llbctgpmExample
+//
+//  Created by Bezzeg Mihály on 2020. 04. 28..
+//  Copyright © 2020. Bezzeg Mihály. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
