3f952b854db7e5081601e67f6a4389043ff84d50
Mihaly Bezzeg <mihaly@brainsandbeards.com>
2020-04-30 21:55:53 +0200

    Setup Carthage repo

---
+++ Carthage/Build/iOS/llbctgpm.framework/Headers/llbctgpm.h
@@ -0,0 +1,19 @@
+//
+//  llbctgpm.h
+//  llbctgpm
+//
+//  Created by Bezzeg Mihály on 2020. 04. 28..
+//  Copyright © 2020. Bezzeg Mihály. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for llbctgpm.
+FOUNDATION_EXPORT double llbctgpmVersionNumber;
+
+//! Project version string for llbctgpm.
+FOUNDATION_EXPORT const unsigned char llbctgpmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <llbctgpm/PublicHeader.h>
+
+
